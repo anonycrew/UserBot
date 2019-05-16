a9bb89bf0fe8ad3863b18b4459cb6c4cc6ba23c0
anonycrew <50693913+anonycrew@users.noreply.github.com>
2019-05-16 15:12:15 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -16,7 +16,7 @@ from userbot import (COUNT_PM, HELPER, LOGGER, LOGGER_GROUP, NOTIF_OFF,
 from userbot.events import register
 
 # ========================= CONSTANTS ============================
-UNAPPROVED_MSG = ("Bleep blop! This is a bot. Don't fret.\n\n"
+UNAPPROVED_MSG = ("Bleep blop! Hello There. Don't fret.\n\n"
                   "My master hasn't approved you to PM."
                   " Please wait for my master to look in, he mostly approves PMs.\n\n"
                   "As far as I know, he doesn't usually approve retards though.")
